9b68349587e55bd6b522ab59ee242b025f7f924b
“no-sato@y-i-group.co.jp” <yigroup08f>
2021-07-10 18:11:18 +0900

    2章 チェックテスト
    2章 チェックテストです。

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,21 @@
+package check;
+
+
+
+public class Check {
+	
+	private String firstname = "Sato";
+	private String lastname = "Norimasa";
+	
+	private void name(String firstname,String lasrtname) {
+		System.out.println(firstname+ lasrtname);
+	}
+	
+	
+	public static void main(String[] args) {
+		Pet ptPet = new Pet(null, null);
+		RobotPet robotPet = new RobotPet(null, null);
+		
+	}
+
+}
