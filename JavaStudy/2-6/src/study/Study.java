cfb1d91391d48a009c52c147915918f22a5d5d24
“no-sato@y-i-group.co.jp” <yigroup08f>
2021-07-10 16:11:25 +0900

    日付操作クラス
    日付操作クラスの提出

--- JavaStudy/2-6/src/study/Study.java
+++ JavaStudy/2-6/src/study/Study.java
@@ -1,4 +1,4 @@
-ackage study;
+package study;
 
 import java.time.Year;
 import java.util.Calendar;
@@ -21,6 +21,8 @@ public class Study {
         Date now = new Date();
 
         // 問① ローカル変数「now」を使用し、本日の日付を表示してください。
+        System.out.println(now);
+        
 
 
         // Calendarクラスのインスタンスを生成
@@ -28,13 +30,21 @@ public class Study {
 
         // 問② 「(Calendar.MONTH) + 数値」の処理は、想定していない値となる可能性があります。
         // その理由をコメントへ記述してください。
-        // [ここへ記述]
+        // ［１３の値を超えた時繰り上げることができない場合があるため。]
         System.out.println(calendar.get(Calendar.YEAR));
         System.out.println(calendar.get(Calendar.MONTH) + 1);
         System.out.println(calendar.get(Calendar.DATE));
         // 問③ 上記の「calendar」を使用し、本日から1年2ヶ月15日先の日付を表示しなさい。
         // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
+        calendar.add(Calendar.YEAR, １);
+        calendar.add(Calendar.MONTH, 2);
+        calendar.add(Calendar.DATE, 15);
+        
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH));
+        System.out.println(calendar.get(Calendar.DATE));
+        
 
 
 
