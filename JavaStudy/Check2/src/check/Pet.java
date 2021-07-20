6aeed4522f03fcda6279a6f9346c82f7e520a19b
“no-sato@y-i-group.co.jp” <yigroup08f>
2021-07-20 21:00:59 +0900

    2章チェックテスト
    2章チェックテストです。

--- JavaStudy/Check2/src/check/Pet.java
+++ JavaStudy/Check2/src/check/Pet.java
@@ -4,7 +4,7 @@ public class Pet {
 
     private String name;
     private String masterName;
-
+    
     public Pet(String name, String masterName) {
         this.name = name;
         this.masterName = masterName;
@@ -19,17 +19,18 @@ public class Pet {
     }
 
     public void introduce() {
-        System.out.println("■僕の名前は" + name + "です");
-        System.out.println("■ご主人様は" + masterName + "です");
+        System.out.println("■僕の名前は" + this.name + "です");
+        System.out.println("■ご主人様は" + this.masterName + "です");
     }
 }
-//親がthis、子がsuperを使う。
+
 class RobotPet extends Pet {
     public RobotPet(String name, String masterName) {
         super(name, masterName);
     }
 
     public void introduce() {
+
         System.out.println("◇私はロボット。名前は" + getName() + "。");
         System.out.println("◇ご主人様は" + getMasterName() + "。");
     }
