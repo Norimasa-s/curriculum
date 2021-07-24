1e4172eec9c5721a307b29052af4dbbc60e065e0
“no-sato@y-i-group.co.jp” <yigroup08f>
2021-07-24 17:41:48 +0900

    2章チェックテスト
    2章チェックテストです。

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -5,17 +5,25 @@ import constants.Constants;
 
 public class Check {
 	
-
+//	課題①
+//	【Check.java】にてフィールド変数firstNameとlastNameを宣言し、
+//	firstName →　自分の名字　lastName →　自分の名前で初期化しなさい。
+//	なお、変数のアクセス修飾子は「private」とする。
 	private static String firstname = "佐藤";
 	private static String lastname = "徳政";
 	
+//	課題②
+//	【Check.java】にてfirstNameとlastNameを引数で受け取って、
+//	連結して表示させるメソッド「printName」を作成しなさい。
+//	作成した関数のアクセス修飾子は「private」とする。
 	private static String printName(String firstname, String lastname) {
 		return firstname + lastname;
 	}
 
 	public static void main(String[] args) {
 	System.out.println("printNameメソッド → " + printName(firstname,lastname));
-
+	//課題③
+	//【Check.java】にてPetクラスとRobotPetクラスをインスタンス化して、下記の完成イメージを出力させなさい。
 
 		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,  Constants.CHECK_CLASS_HOGE);
 		pet.introduce();
