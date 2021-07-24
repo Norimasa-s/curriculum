3f139684565653a832bf83f437727f067ad37e4c
“no-sato@y-i-group.co.jp” <yigroup08f>
2021-07-24 19:14:28 +0900

    Check2コメント入り
    Check2コメント入り

---
+++ JavaStudy/Check2コメント入り/Check.java
@@ -0,0 +1,49 @@
+package check;
+import constants.Constants;
+
+
+
+public class Check {
+	
+	//メソッドや変数に付けることによって、
+	//staticはインスタンス化しなくても、クラスから直接メソッドや変数にアクセスすることができるようになる。
+	//↓フィールド変数を定義して、他のところでも使えるようになるため。
+	private static String firstname = "佐藤";
+	private static String lastname = "徳政";
+	//課題②
+	//【Check.java】にてfirstNameとlastNameを引数で受け取って、
+	//連結して表示させるメソッド「printName」を作成しなさい。
+	//作成した関数のアクセス修飾子は「private」とする。
+	private static String printName(String firstname, String lastname) {
+		return firstname + lastname;
+	}
+
+	public static void main(String[] args) {
+	System.out.println("printNameメソッド → " + printName(firstname,lastname));
+	//こっちの方の引数は渡している。
+
+		//課題③
+		//【Check.java】にてPetクラスとRobotPetクラスをインスタンス化して、下記の完成イメージを出力させなさい。
+		//このインスタンスはPetクラスのコンストラクタへ
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,  Constants.CHECK_CLASS_HOGE);
+		pet.introduce();
+		//インスタンス
+		// ① クラス名
+		// ② インスタンス名（変数名）
+		// ③ 「=」で結んで、new演算子
+		// ④ コンストラクタ名
+		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		robotPet.introduce();
+	}
+		
+		
+		
+}
+	
+	
+		
+		
+
+
+
+
---
+++ JavaStudy/Check2コメント入り/Pet.java
@@ -0,0 +1,41 @@
+package check;
+
+public class Pet {
+//フィールド
+    private String name;
+    private String masterName;
+    
+//コンストラクタ
+    public Pet(String name, String masterName) {
+        this.name = name;
+        this.masterName = masterName;
+    }
+//外のフィールドからアクセスするためにgetメソッドを使っている。
+    protected String getName() {
+        return name;
+    }
+// protectedはクラスと継承されたサブクラスのみで使われる。
+    protected String getMasterName() {
+        return masterName;
+    }
+
+    public void introduce() {
+        System.out.println("■僕の名前は" + this.name + "です");
+        System.out.println("■ご主人様は" + this.masterName + "です");
+    }
+}
+//親がsuperを使う。
+//extendsで継承している。この場合はPetクラスが親クラス。
+class RobotPet extends Pet {
+    public RobotPet(String name, String masterName) {
+        // 親クラスと明示するためsuperがついている。
+        super(name, masterName);
+    }
+
+    public void introduce() {
+        //この場合はオーバーライド。親クラスのメソッドを子クラスで上書き。
+        // 戻り値がないためvoidがある。
+        System.out.println("◇私はロボット。名前は" + getName() + "。");
+        System.out.println("◇ご主人様は" + getMasterName() + "。");
+    }
+}
